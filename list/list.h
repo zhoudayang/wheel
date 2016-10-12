56b3ee4bc926084fede920402be74d3da422dde1
zhoudayang <zhoudayang2@163.com>
2016-10-12 22:46:52 +0800

    add minus operator implement

--- list/list.h
+++ list/list.h
@@ -11,82 +11,90 @@ template<typename Object>
 class List {
 private:
     struct Node {
-        Node(const Object& d = Object{}, Node* p = nullptr, Node* n = nullptr)
-                :data(d),
-                 prev(p),
-                 next(n)
-        {
+        Node(const Object &d = Object{}, Node *p = nullptr, Node *n = nullptr)
+                : data(d),
+                  prev(p),
+                  next(n) {
 
         }
 
-        Node(Object&& d, Node* p = nullptr, Node* n = nullptr)
-                :data(d),
-                 prev(p),
-                 next(n)
-        {
-
+        Node(Object &&d, Node *p = nullptr, Node *n = nullptr)
+                : data(d),
+                  prev(p),
+                  next(n) {
+//
         }
+
         //data
         Object data;
         //prev pointer
-        Node* prev;
+        Node *prev;
         //next pointer
-        Node* next;
+        Node *next;
 
     };
+
 public:
 
     class const_iterator {
     public:
         const_iterator()
-                :current(nullptr) { }
+                : current(nullptr) {}
+
         //解引用
-        Object& operator*() const
-        {
+        Object &operator*() const {
             return retrieve();
         }
 
-        const Object * operator->() const{
+        const Object *operator->() const {
             return retriveAsPtr();
         }
 
-        const_iterator& operator++()
-        {
+        const_iterator &operator++() {
             current = current->next;
             return *this;
         }
 
+
         //返回的是局部变量，不能为引用类型
-        const_iterator operator++(int)
-        {
+        const_iterator operator++(int) {
             const_iterator old = *this;
             ++(*this);
             return old;
         }
 
-        bool operator==(const const_iterator& rhs) const
-        {
-            return current==rhs.current;
+        const_iterator &operator--() {
+            current = current->prev;
+            return *this;
         }
 
-        bool operator!=(const const_iterator& rhs) const
-        {
-            return !(*this==rhs);
+        const_iterator operator--(int) {
+            const_iterator old = *this;
+            --(*this);
+            return old;
         }
+
+        bool operator==(const const_iterator &rhs) const {
+            return current == rhs.current;
+        }
+
+        bool operator!=(const const_iterator &rhs) const {
+            return !(*this == rhs);
+        }
+
     protected:
-        Node* current;
+        Node *current;
 
-        Object& retrieve() const
-        {
+        Object &retrieve() const {
             return current->data;
         }
 
-        Object * retriveAsPtr() const{
+        Object *retriveAsPtr() const {
             return &(current->data);
         }
 
-        const_iterator(Node* p)
-                :current(p) { }
+        const_iterator(Node *p)
+                : current(p) {}
 
         //使得List<Object>可以访问protected作用域下的构造函数
         friend class List<Object>;
@@ -94,40 +102,47 @@ public:
 
     class iterator : public const_iterator {
     public:
-        iterator() { }
+        iterator() {}
 
-        Object& operator*()
-        {
+        Object &operator*() {
             return const_iterator::retrieve();
         }
 
-        Object * operator->(){
+        Object *operator->() {
             return const_iterator::retriveAsPtr();
         }
 
-        iterator operator++()
-        {
+        iterator operator++() {
             this->current = this->current->next;
             return *this;
         }
 
         //返回的是局部变量，不能为引用类型
-        iterator operator++(int)
-        {
+        iterator operator++(int) {
             iterator old = *this;
             ++(*this);
             return old;
         }
 
+        iterator &operator--() {
+            this->current = this->current->prev;
+            return *this;
+        }
+
+        iterator operator--(int) {
+            iterator old = *this;
+            --(*this);
+            return old;
+        }
+
     protected:
-        iterator(Node* p)
-                :const_iterator(p) { }
+        iterator(Node *p)
+                : const_iterator(p) {}
 
         friend class List<Object>;
     };
 
-    void init()
-    {
+    void init() {
         theSize = 0;
         head = new Node;
         tail = new Node;
@@ -135,156 +150,134 @@ public:
         tail->prev = head;
     }
 
-    List()
-    {
+    List() {
         init();
     };
 
-    List(const List& rhs)
-    {
+    List(const List &rhs) {
         init();
-        for (auto& x:rhs)
+        //这里有一个auto调用
+        for (auto &x:rhs)
         {
             push_back(x);
         }
     }
 
-    List(List&& rhs)
-            :theSize(rhs.theSize),
-             head(rhs.head),
-             tail(rhs.tail)
-    {
+    List(List &&rhs)
+            : theSize(rhs.theSize),
+              head(rhs.head),
+              tail(rhs.tail) {
         theSize = 0;
         rhs.head = nullptr;
         rhs.tail = nullptr;
     }
 
-    ~List()
-    {
+    ~List() {
         clear();
         delete head;
         delete tail;
     }
 
-    List& operator=(const List& rhs)
-    {
+    List &operator=(const List &rhs) {
         List copy = rhs;
         std::swap(copy, rhs);
         return *this;
     }
-    List& operator=(List&& rhs)
-    {
+
+    List &operator=(List &&rhs) {
         std::swap(theSize, rhs.theSize);
         std::swap(head, rhs.head);
         std::swap(tail, rhs.tail);
         return *this;
     }
 
-    iterator begin()
-    {
+    iterator begin() {
         return {head->next};
     }
 
-    const_iterator begin() const
-    {
+    const_iterator begin() const {
         return {head->next};
     }
 
-    iterator end()
-    {
-        return {tail};
-    }
-    const_iterator end() const
-    {
+    iterator end() {
         return {tail};
     }
 
-    int size() const
-    {
+    const_iterator end() const {
+        return {tail};
+    }
+
+    int size() const {
         return theSize;
     }
 
-    bool empty() const
-    {
-        return size()==0;
+    bool empty() const {
+        return size() == 0;
     }
 
-    void clear()
-    {
+    void clear() {
         while (!empty())
         {
             pop_front();
         }
     }
 
-    Object& front()
-    {
-        return *begin();
-    }
-    const Object& front() const
-    {
+    Object &front() {
         return *begin();
     }
 
-    Object& back()
-    {
-        return --*end();
+    const Object &front() const {
+        return *begin();
     }
 
-    const Object& back() const
-    {
-        return --*end();
+    Object &back() {
+        return *--end();
     }
 
-    void push_front(const Object& x)
-    {
+    const Object &back() const {
+        return *--end();
+    }
+
+    void push_front(const Object &x) {
         insert(begin(), x);
     }
 
-    void push_front(Object&& x)
-    {
+    void push_front(Object &&x) {
         insert(begin(), std::move(x));
     }
 
-    void push_back(const Object& x)
-    {
+    void push_back(const Object &x) {
         insert(end(), x);
     }
 
-    void push_back(Object&& x)
-    {
+    void push_back(Object &&x) {
         insert(end(), std::move(x));
     }
 
-    void pop_front()
-    {
+    void pop_front() {
         erase(begin());
     }
 
-    void pop_back()
-    {
+    void pop_back() {
         erase(--end());
     }
 
     //在it之前插入x
-    iterator insert(iterator it, const Object& x)
-    {
-        Node* p = it.current;
+    iterator insert(iterator it, const Object &x) {
+        Node *p = it.current;
         ++theSize;
         return {p->prev = p->prev->next = new Node(x, p->prev, p)};
     }
 
-    iterator insert(iterator it, Object&& x)
-    {
-        Node* p = it.current;
+    iterator insert(iterator it, Object &&x) {
+        Node *p = it.current;
         ++theSize;
         return {p->prev = p->prev->next = new Node(std::move(x), p->prev, p)};
     }
 
     //删除it对应的节点
-    iterator erase(iterator it)
-    {
-        Node* p = it.current;
+    iterator erase(iterator it) {
+        Node *p = it.current;
         iterator ret{p->next};
         p->prev->next = p->next;
         p->next->prev = p->prev;
@@ -293,18 +286,17 @@ public:
         return ret;
     }
 
-    iterator erase(iterator from, iterator to)
-    {
+    iterator erase(iterator from, iterator to) {
         //此处不能采取itr++, 因为在erase之后,itr就失效了
-        for (iterator itr = from; itr!=to;)
+        for (iterator itr = from; itr != to;)
             itr = erase(itr);
         return to;
     }
 
 private:
     int theSize;
-    Node* head;
-    Node* tail;
+    Node *head;
+    Node *tail;
 
 };
 
--- list/main.cpp
+++ list/main.cpp
@@ -11,42 +11,35 @@ using std::endl;
 class test {
 public:
     test()
-            :
-            val_(0)
-    {
+            : val_(0) {
         //cout << "costructor" << endl;
     }
 
     explicit test(int val)
-            :val_(val)
-    {
+            : val_(val) {
         //cout << "constructor" << endl;
     }
 
-    test(const test& elem)
+    test(const test &elem)
             :
-            val_(elem.val_)
-    {
+            val_(elem.val_) {
         //cout << "copy constructor" << endl;
     }
 
-    test(test&& elem)
+    test(test &&elem)
             :
-            val_(std::move(elem.val_))
-    {
+            val_(std::move(elem.val_)) {
         //cout << "move copy constructor" << endl;
     }
 
-    test& operator=(const test& elem)
-    {
+    test &operator=(const test &elem) {
         val_ = elem.val_;
         //cout << "copy assignment function" << endl;
         return *this;
     }
 
-    test& operator=(test&& elem)
-    {
-        if (this!=&elem)
+    test &operator=(test &&elem) {
+        if (this != &elem)
         {
             val_ = std::move(elem.val_);
         }
@@ -54,13 +47,11 @@ public:
         return *this;
     }
 
-    void print() const
-    {
+    void print() const {
         cout << "value= " << val_ << endl;
     }
 
-    ~test()
-    {
+    ~test() {
         //cout << "destructor" << endl;
     }
 
@@ -68,49 +59,55 @@ private:
     int val_;
 };
 
-int main()
-{
+int main() {
     List<test> container;
 //    上面涉及到这几次函数调用：首先创建零参数的test实例，然后利用复制构造函数赋值给Node中的成员data，复制完毕，将临时对象析构
 //    因为初始化了head 和 tail,　所以上述调用进行了两次，最后将所有的node 中的data析构
 
-    for (int i = 0; i<1; ++i)
+    for (int i = 0; i < 1; ++i)
     {
         container.push_back(test());
     }
 
     container.begin()->print();
 
-    assert(container.size()==1);
+    assert(container.size() == 1);
 
-    for(int i=0;i<10000;i++)
+    for (int i = 0; i < 10000; i++)
         container.push_back(test(i));
-    assert(container.size() ==10001);
+    assert(container.size() == 10001);
 
 
     container.clear();
 
-    assert(container.size()==0);
+    assert(container.size() == 0);
 
-    for(auto val:{1,2,3,4,5,6,7,8}){
+    for (auto val:{1, 2, 3, 4, 5, 6, 7, 8})
+    {
         container.push_back(test(val));
     }
 
-    assert(container.size()==8);
+    assert(container.size() == 8);
 
     auto it = container.begin();
     ++it;
     ++it;
     it = container.erase(it);
-    cout<<endl;
-    for(auto it=container.begin();it!=container.end();++it)
+    cout << endl;
+    for (auto it = container.begin(); it != container.end(); ++it)
         it->print();
-    cout<<endl;
-    container.insert(it,test(1090));
-    for(auto it=container.begin();it!=container.end();++it)
+    cout << endl;
+    container.insert(it, test(1090));
+    for (auto it = container.begin(); it != container.end(); ++it)
         it->print();
 
+    List<test> list = container;
+    cout<<list.size()<<endl;
 
+    cout<<endl;
+
+    list.back().print();
+    list.front().print();
 
     return 0;
 }
