8835e9556b9854507ab19bc0048e3c68326452bf
zhoudayang <zhoudayang2@163.com>
2016-10-16 13:04:59 +0800

    change vector test case

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,68 +1,47 @@
+
 #include "Vector.h"
-
 #include <iostream>
+#include <algorithm>
+using namespace std;
+void print( const Vector<Vector<int>> arr )
+{
+    int N = arr.size( );
 
-
-using std::cout;
-using std::endl;
-
-
-class test {
-public:
-    test() :
-            val_(0) {
-        cout << "costructor" << endl;
-    }
-
-    test(const test &elem) :
-            val_(elem.val_) {
-        cout << "copy constructor" << endl;
-    }
-
-    test(test &&elem) :
-            val_(std::move(elem.val_)) {
-        cout << "move copy constructor" << endl;
-    }
-
-    test &operator=(const test &elem) {
-        val_ = elem.val_;
-        cout << "copy assignment function" << endl;
-        return *this;
-    }
-
-    test &operator=(test &&elem) {
-        if (this != &elem)
-        {
-            val_ = std::move(elem.val_);
-        }
-        cout << "move copy assignment function" << endl;
-        return *this;
-    }
-
-    void print() const {
-        cout << "val: " << val_ << endl;
-    }
-
-    ~test(){
-        cout<<"destructor"<<endl;
-    }
-
-private:
-    int val_;
-};
-
-
-int main() {
-    Vector<test> container;
-    for (int i = 0; i < 1; ++i)
+    for( int i = 0; i < N; ++i )
     {
-        container.push_back(test());
-    }
-    for(auto it = container.begin();it!=container.end();++it){
-        it->print();
-    }
+        cout << "arr[" << i << "]:";
 
+        for( int j = 0; j < arr[ i ].size( ); ++j )
+            cout << " " << arr[ i ][ j ];
 
+        cout << endl;
+    }
+}
+class CompareVector
+{
+public:
+    bool operator() ( const Vector<int> & lhs, const Vector<int> & rhs ) const
+    { return lhs.size( ) < rhs.size( ); }
+};
+int main( )
+{
+    const int N = 20;
+    Vector<Vector<int>> arr( N );
+    Vector<int> v;
+
+    for( int i = N - 1; i > 0; --i )
+    {
+        v.push_back( i );
+        arr[ i ] = v;
+    }
+    print( arr );
+
+    clock_t start = clock( );
+    std::sort( begin( arr ), end( arr ), CompareVector{ } );
+    clock_t end = clock( );
+    cout << "Sorting time: " << ( end - start ) << endl;
+
+    print( arr );
 
     return 0;
 }
